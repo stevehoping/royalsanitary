a383c969ef5f28764d5ab44278c73a7ef196dbde
rajive0086 <zink.bitart@gmail.com>
2020-09-27 14:32:48 +0530

    Todos controller added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TodosController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('todos', [TodosController::class, 'index']);
